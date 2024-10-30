4d4d9688d1be14aaf62403fcbcf9fbc06253e2b5
Aman Dwivedi <amandwivedi71124.com>
2024-10-30 14:23:48 +0530

    added

--- routes/habitRoutes.js
+++ routes/habitRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { getHabits, createHabit, updateHabit, deleteHabit } = require('../controllers/habitController');
-const { protect } = require('../middleware/authmiddleware');
+const { protect } = require('../middleware/authMiddleware');
 
 const router = express.Router();
 
